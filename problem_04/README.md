e98e75a10bc1b6c27980ff4857c50360690d07c4
marouanaddou <marwanaddou123@gmail.com>
2025-02-20 23:54:22 +0100

    Invert Binary Tree using DFS and queue for level-order traversal

---
+++ problem_04/README.md
@@ -0,0 +1,38 @@
+    💡 Invert Binary Tree 💡
+
+**💡 Problem Description**
+Given the `root` of a binary tree, invert the tree, and return its root.
+
+Example 1:
+![image](https://assets.leetcode.com/uploads/2021/03/14/invert1-tree.jpg)
+
+
+    Input: root = [4,2,7,1,3,6,9]
+    Output: [4,7,2,9,6,3,1]
+
+Example 2:
+![image](https://assets.leetcode.com/uploads/2021/03/14/invert2-tree.jpg)
+
+    Input: root = [2,1,3]
+    Output: [2,3,1]
+
+Example 3:
+
+    Input: root = []
+    Output: []
+
+Constraints:
+
+- The number of nodes in the tree is in the range `[0, 100]`.
+- `-100 <= Node.val <= 100`
+
+**💡 Approach**
+
+The goal is to invert a binary tree by swapping the left and right child of each node.
+To achieve this, we use `BFS` `(level-order traversal)` with a `queue` to process nodes iteratively.
+
+**Steps**
+- Use a queue to traverse the tree level by level.
+- Swap the left and right child of each node.
+- Push non-null children into the queue to continue processing.
+- Continue until all nodes are inverted.
---
+++ problem_04/invertBinaryTree.cpp
@@ -0,0 +1,72 @@
+
+
+# include <iostream>
+# include <queue>
+# include <algorithm>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+    public:
+        TreeNode* invertTree(TreeNode* root); 
+        TreeNode* invertTree1(TreeNode* root); 
+};
+    // first code
+TreeNode* Solution::invertTree1(TreeNode* root) {
+    if ( root == nullptr ) return nullptr;
+    std::queue< TreeNode *> q;
+    q.push( root );
+    int size;
+    while ( !q.empty() ) {
+        size = q.size();
+        for ( int i = 0; i < size; i++ ) {
+            TreeNode* tmp = q.front();
+            q.pop();
+            if ( tmp->left != nullptr && tmp->right != nullptr ) {
+                TreeNode* tmp1 = tmp->left;
+                tmp->left = tmp->right;
+                tmp->right = tmp1;
+            }
+            else if ( tmp->left != nullptr && tmp->right == nullptr ) {
+                tmp->right = tmp->left;
+                tmp->left = nullptr;
+            }
+            else if ( tmp->right != nullptr && tmp->left == nullptr ) {
+                tmp->left = tmp->right;
+                tmp->right = nullptr;
+            }
+            if ( tmp->left != nullptr ) q.push( tmp->left );
+            if ( tmp->right != nullptr ) q.push( tmp->right );
+        }
+    }
+    return root;
+}
+
+    // Use std::swap to invert the left and right children of the node
+TreeNode* Solution::invertTree(TreeNode* root) {
+    if ( root == nullptr ) return nullptr;
+    std::queue< TreeNode *> q;
+    q.push( root );
+    int size;
+    while ( !q.empty() ) {
+        size = q.size();
+        for ( int i = 0; i < size; i++ ) {
+            TreeNode* tmp = q.front();
+            q.pop();
+            std::swap( tmp->left , tmp->right );
+            if ( tmp->left != nullptr ) q.push( tmp->left );
+            if ( tmp->right != nullptr ) q.push( tmp->right );
+        }
+    }
+    return root;
+}
+
