9c4ca89f19a687d84bfc183d6990c9bc5491529e
marouanaddou <marwanaddou123@gmail.com>
2025-03-21 21:10:33 +0000

    Find whether the number x is present in the matrix or not

---
+++ Search_in_a_sorted_Matrix/Search_in_a_sorted_Matrix.cpp
@@ -0,0 +1,20 @@
+
+
+# include <iostream>
+# include <vector>
+# include <algorithm>
+
+using namespace std;
+
+class Solution {
+    public:
+      // Function to search a given number in row-column sorted matrix.
+      bool searchMatrix(vector<vector<int>> &mat, int x) {
+          for ( int i = 0; i < mat.size(); i++ ) {
+              if ( find( mat[i].begin(), mat[i].end(), x ) != mat[i].end() ) return true;
+          }
+          return false;
+          // code here
+      }
+  };
+int main(){}
